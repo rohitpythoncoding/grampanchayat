d35312a6a98ab0d71c62cbf4d9dfe68fe46c4949
Rohit Shinde <rohitpythoncoding@gmail.com>
2018-11-20 00:32:42 +0530

    change in settings file

--- grampanchayatProject/settings.py
+++ grampanchayatProject/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'grampanchayatSaravali/static')
+print("base: ", BASE_DIR)
+print("STATIC_ROOT: ", STATIC_ROOT)
 
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
@@ -133,3 +135,4 @@ STATICFILES_DIRS = (
     ),
 )
 
+print("STATICFILES_DIRS: ", os.path.dirname(__file__))
